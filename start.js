fcbe0b9832e26353f41112d8b2c2aecd645d2516
Joseph Alves <databentpx@gmail.com>
2015-02-15 13:11:16 -0500

    Correct start file

--- start.js
+++ start.js
@@ -1,3 +1,11 @@
-require('./server').listen(1337, function () {
-    console.log('Server started on port 1337');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/flash-cards');
+
+var server = require('./server');
+
+mongoose.connection.once('open', function () {
+    server.listen(1337, function () {
+        console.log('Server started on port 1337');
+    });
 });
